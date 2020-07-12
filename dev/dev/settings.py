8a0e9a0a6ad66c68af1a5846a5ea2ed22ad5702c
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-12 16:56:56 +0300

    bg:unexposing_the_secret_key:backend

--- dev/dev/settings.py
+++ dev/dev/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$n-kvo%+z+-p&xj(t7j%u9r(k91tj@ia+^8vq^w=#snyc_8=v3'
+from decouple import config 
+SECRET_KEY = config(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
