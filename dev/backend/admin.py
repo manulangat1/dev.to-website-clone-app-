8b56a7b49f1d60bc3f3e9491f991df85e05eed71
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-12 17:30:45 +0300

    ch:adding_post_model:backend

--- dev/backend/admin.py
+++ dev/backend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.db import  models
+from martor.widgets import AdminMartorWidget
+from .models import Post
 
 # Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField:{'widget':AdminMartorWidget}
+    }
+
+admin.site.register(Post,PostModelAdmin)
--- dev/backend/models.py
+++ dev/backend/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from martor.models import MartorField 
 # Create your models here.
+
+class BaseModel(models.Model):
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField()
+
+class Post(BaseModel):
+    title = models.CharField(max_length=70)
+    body = MartorField()
+
+    def __str__(self):
+        return self.title
+
+
+
+
