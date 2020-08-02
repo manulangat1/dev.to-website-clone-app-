fcdd09686f0664455080c2d083e7180310ea1afd
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-02 15:58:20 +0300

    ft:adding_resgister_component:frontend

---
+++ dev/frontend/src/components/auth/Register.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class Register extends React.Component{
+    state = {
+        username,
+        password,
+        password2,
+        bio,
+        email
+    }
+    onChange = e => this.setState({[e.target.name]:e.target.value})
+    onSubmit = e => {
+        e.preventDefault()
+        const { username,password,password2,bio,email} = this.state
+        console.log(username,password,password2,bio,email)
+    }
+    render(){
+        const { username,password,password2,bio,email} = this.state
+        return(
+            <section id="login">
+                <h1>Login </h1>
+                <div className="form">
+                    <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Username</label>
+                        <input type="text" value={username} name="username" className="form-control" onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label>password</label>
+                        <input type="password" value={password} name="password" className="form-control" onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label>password</label>
+                        <input type="password" value={password2} name="password2" className="form-control" onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label>email</label>
+                        <input type="email" value={email} name="email" className="form-control" onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <label>Bio</label>
+                        <input type="text" value={bio} name="bio" className="form-control" onChange={this.onChange} />
+                    </div>
+                    <input type="submit" className="primary-btn" Value="Sign in"/>
+                    </form>
+                </div>
+            </section>
+        )
+    }
+}
+const mapStateToProps = state => ({
+
+})
+// export default connect(mapStateToProps,{})(Register)
+export default Register
