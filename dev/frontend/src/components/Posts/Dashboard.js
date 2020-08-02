f35914b559bd11ba37c72775eca7f7ebd0336c4a
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-02 22:59:22 +0300

    ft:not_done_yest:frontend

--- dev/frontend/src/components/Posts/Dashboard.js
+++ dev/frontend/src/components/Posts/Dashboard.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
-
+import Post from './Post'
+import Sticky from 'react-stickynode';
 class Dashboard extends React.Component{
+    sidebar = document.getElementById("sidebar")
+    constructor(){
+        super()
+        window.onscroll = () => {
+            
+            console.log("hdjd")
+            if(document.documentElement.scrollHeight - document.documentElement.scrollTop === document.documentElement.clientHeight ){
+                console.log("pdhdh")
+                console.log(document.documentElement.scrollHeight + document.documentElement.scrollHeight)
+                this.sidebar.style({height:document.documentElement.scrollHeight + document.documentElement.scrollHeight})
+                console.log(this.sidebar)
+            }
+        }
+    }
     render(){
         return(
-            <section>
-
+            <section id="dash">
+                <div className="container">
+                <Sticky enabled={true} top={50} bottomBoundary={1200} innerClass="sidepanel" releasedClass="sidepanel">
+                        {/* <YourComponent/> */}
+                        <h1>ppp</h1>
+                </Sticky>
+                    {/* <div id="page-wrap">
+                    <div id="sidebar">
+                        <h1>hey</h1>
+                    </div>
+                    </div> */}
+                    <div className="main">
+                    <Post />
+                    </div>
+                    
+                </div>
             </section>
         )
     }
